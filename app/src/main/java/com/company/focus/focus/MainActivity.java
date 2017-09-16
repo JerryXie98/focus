e0e4d10636c045c6f54a2e53b401dc2d7774726f
Jerry Xie <jerry_xie@hotmail.com>
2017-09-16 17:45:31 -0400

    Refactored UI elements.

--- app/src/main/java/com/company/focus/focus/MainActivity.java
+++ app/src/main/java/com/company/focus/focus/MainActivity.java
@@ -5,9 +5,11 @@ package com.company.focus.focus;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
@@ -16,44 +18,80 @@ import com.spotify.sdk.android.authentication.AuthenticationResponse;
 import com.spotify.sdk.android.player.Config;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
 import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Metadata;
 import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerEvent;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
 
-import android.widget.TextView;
-
-import static com.company.focus.focus.R.id.tv;
-
 public class MainActivity extends Activity implements
         SpotifyPlayer.NotificationCallback, ConnectionStateCallback
 {
-
-    // TODO: Replace with your client ID
     private static final String CLIENT_ID = "0a8b3b2522e14c8ebc0bbe3236f8f446";
-    // TODO: Replace with your redirect URI
     private static final String REDIRECT_URI = "focus-app-login://callback";
 
     // Request code that will be used to verify if the result comes from correct activity
-    // Can be any integer
     private static final int REQUEST_CODE = 1337;
 
     private Player mPlayer;
+    public Metadata.Track currentTrack;
+
+    TextView currentSong;
+    Button skipButton, playButton;
 
     public String upbeatSong = "spotify:track:4HW5kSQ8M2IQWZhSxERvla";
+    public String lowkeySong = "spotify:track:6ZzQ76i9WDdVmwVeDQIcJz";
+
+    public void updateView() {
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.skipToNext(null);
+                if (currentSong != null) {
+                    currentSong.setText(String.format("%s by %s from the album: %s is now playing.",
+                            currentTrack.name, currentTrack.artistName, currentTrack.albumName));
+                }
+                else {
+                    currentSong.setText("Something Wrong");
+                }
+            }
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.setRepeat(null, true);
+                mPlayer.playUri(null, upbeatSong, 0, 0);
+                mPlayer.queue(null, lowkeySong);
+            }
+        });
+    }
 
     @Override
     //Main Function (Shit that matters)
     public void onLoggedIn() {
         Log.d("MainActivity", "User logged in");
-        //mPlayer.playUri(null, "spotify:track:2TpxZ7JUBn3uw46aR7qd6V", 0, 0);
-        mPlayer.playUri(null, upbeatSong, 0, 0);
+        updateView();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout lin1 = (LinearLayout) findViewById(R.id.l1);
+
+        skipButton = new Button(getApplicationContext());
+        skipButton.setText("Skip Song");
+        lin1.addView(skipButton);
+
+        playButton = new Button(getApplicationContext());
+        playButton.setText("Play");
+        lin1.addView(playButton);
+
+        currentSong = new TextView(getApplicationContext());
+        lin1.addView(currentSong);
+
 
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
                 AuthenticationResponse.Type.TOKEN,
